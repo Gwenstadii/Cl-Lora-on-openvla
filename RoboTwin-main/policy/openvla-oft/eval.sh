de08afb93e1b0d5238cb9e4134a59cb152875af0
Gwenstadii <2784409183@qq.com>
2026-08-13 18:44:34 +0800

    评估默认关闭视频录制，加速 debug 阶段评估

--- RoboTwin-main/policy/openvla-oft/eval.sh
+++ RoboTwin-main/policy/openvla-oft/eval.sh
@@ -25,7 +25,8 @@ python script/eval_policy.py --config policy/${policy_name}/deploy_policy.yml \
     --seed ${seed} \
     --policy_name ${policy_name} \
     --unnorm_key ${unnorm_key} \
-    --eval_task_id ${eval_task_id}
+    --eval_task_id ${eval_task_id} \
+    --eval_video_log False
 
 # example usage
 # bash eval.sh move_can_pot demo_randomized ckpt_path 0 5 aloha_move_can_pot_builder
